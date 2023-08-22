642f7072f46f076c4be1130c65166de7c764d1a1
Aarti Lanke <aarti.22020248@viit.ac.in>
2023-08-23 00:54:03 +0530

    Add: update UI

--- src/app/page.js
+++ src/app/page.js
@@ -11,118 +11,37 @@ const socialMediaLinks = [
   { name: 'Twitter', icon: '/twitter-icon.svg', url: 'https://twitter.com/worqhat' },
   { name: 'GitHub', icon: '/github-icon.svg', url: 'https://github.com/worqhat' },
 ];
-
 export default function Home() {
-  const [prompt, setPrompt] = useState('');
-  const [loading, setLoading] = useState(false); // Add loading state
-  // const [generatedLink, setGeneratedLink] = useState(''); // Use the dummy link
-
-  const [copySuccess, setCopySuccess] = useState(false);
-
-  // Replace the useEffect and loading state with dummy image link
-   const generatedLink = 'https://worqhat.s3.ap-south-1.amazonaws.com/outputs/1692603817264.png';
-
-  useEffect(() => {
-    // Move the API request logic here
-    if (prompt && loading) {
-      const data = JSON.stringify({
-        "prompt": [prompt]
-      });
-
-      const xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
-
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-          setLoading(false); // Set loading to false after receiving the response
-          //setResponseText(this.responseText); // Store the API response
-          const response = JSON.parse(this.responseText);
-          const content = response.content; // Extract the "content" value
-          setGeneratedLink(content); // Store the generated link
-          console.log(this.responseText);
-        }
-      });
-
-      xhr.open("POST", "https://api.worqhat.com/api/ai/images/generate/v2");
-      xhr.setRequestHeader("Authorization", "Bearer sk-5ad64bcfb0d441bdbd6c8ab9f07b3647");
-      xhr.setRequestHeader("Content-Type", "application/json");
-
-      xhr.send(data);
-    }
-  }, [prompt, loading]);
-
-  const handlePromptChange = (event) => {
-    setPrompt(event.target.value);
-  };
-  const handleGenerateImage = () => {
-    setLoading(true); // Set loading to true when "Generate Image" button is clicked
-    setGeneratedLink(''); // Clear the generatedLink when generating a new image
-    setCopySuccess(false); // Reset the copy success state
-    setCopySuccess(true);
-
-  };
-  // const handleDownloadImage = () => {
-  //   if (generatedLink) {
-  //     const link = document.createElement('a');
-  //     link.href = generatedLink;
-  //     link.download = 'generated_image.png'; // Set the desired file name
-  //     link.target = '_blank';
-  //     link.click();
-  //   }
-  // };
-
-  const handleDownloadImage = () => {
-    if (generatedLink) {
-      fetch(generatedLink)
-        .then(response => response.blob())
-        .then(blob => {
-          const url = URL.createObjectURL(blob);
-          const link = document.createElement('a');
-          link.href = url;
-          link.download = 'generated_image.png'; // Set the desired file name
-          link.click();
-          URL.revokeObjectURL(url);
-        })
-        .catch(error => {
-          console.error('Error downloading image:', error);
-          // Handle error if downloading fails
-        });
-    }
-  };
+ 
   
-
-  const handleCopylink = () => {
-
-    if (generatedLink) {
-      navigator.clipboard.writeText(generatedLink);
-      setCopySuccess(true); // Set copy success to true
-      setTimeout(() => {
-        setCopySuccess(false); // Reset copy success after 5 seconds
-      }, 3000); // 5000 milliseconds = 5 seconds
+  useEffect(() => {
+    function handleFileInputChange(event) {
+    const selectedFile = event.target.files[0]; // Get the selected file
+    if (!selectedFile) {
+      return;
     }
-  };
-  const handleCopyImage = () => {
-    if (generatedLink) {
-      fetch(generatedLink)
-        .then(response => response.blob())
-        .then(blob => {
-          navigator.clipboard.write([
-            new ClipboardItem({
-              [blob.type]: blob
-            })
-          ]);
+  
+      const form = new FormData();
+      form.append("audio", selectedFile);
 
-          setCopySuccess(true); // Set copy success to true
-          setTimeout(() => {
-            setCopySuccess(false); // Reset copy success after 5 seconds
-          }, 3000); // 5000 milliseconds = 5 seconds
-        })
-        .catch(error => {
-          console.error('Error copying image:', error);
-          // Handle error if copying fails
-        });
+      const options = {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer sk-3ce895380dec49f7ae3c220c1dc1c3fe',
+          'Content-Type': 'multipart/form-data'
+        },
+        body:form,
+      };
+
+      // options.body = form;
+
+      fetch('https://api.worqhat.com/api/ai/speech-text', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
     }
-  };
+  });
 
   return (
 
@@ -142,89 +61,50 @@ export default function Home() {
 
 
         <div className={`${styles.heading}`}>
-          AI Image Generator by WorqHat
+          Speech Extraction
         </div>
 
-        <div className={`${styles.flex1}`}>
 
-          <div className={`${styles.headings}`}>User Input</div>
+        <div style={{ color: "gray", textAlign: "center", content: "center", marginTop: "30px", fontSize: "1.5rem", padding: "25px" }}>
+          Our Speech Extraction API endpoint is used to extract text from audio files.</div>
 
-          <div className={`${styles.inputBox}`}>
-            {/* <input
-              type="text"
-              value={prompt}
-              onChange={handlePromptChange}
-              placeholder="Describe what you want and directly click the 'Generate' button to  Generate Image"
-              className={`${styles.inputField}`}
-              
-              style={{ color: 'black' }}
-            /> */}
-            <input
-              type="text"
-              value={prompt}
-              onChange={handlePromptChange}
-              onFocus={() => {
-                const inputField = document.querySelector(`.${styles.inputField}`);
-                inputField.style.border = '1px solid #ff0000'; // Change to the desired border color
-              }}
-              onBlur={() => {
-                const inputField = document.querySelector(`.${styles.inputField}`);
-                inputField.style.border = '1px solid #000000'; // Change to the default border color
-              }}
-              placeholder="Describe what you want and directly click the 'Generate Image' button "
-              className={`${styles.inputField}`}
-              style={{ color: 'black' }}
-            />
 
-            <button
-              className={`${styles.generateButton}`}
-              onClick={() => {
-                setLoading(true);
-                setPrompt(prompt);
-              }}
-            >
-              {loading ? 'Generating...' : 'Generate Image'}
-            </button>
+        <div className={`${styles.headings}`}>Upload</div>
+        <div className={`${styles.boxWithContent}`}>
+          <div className={`${styles.centerContent}`}>
+            <p className={` ${styles.ptags}`}>
+              Drop an audio file here
+            </p>
+            <p className={`${styles.ptag}`}>
+              or click to browse
+            </p>
           </div>
-
-          <div className={`${styles.headings}`}>Response</div>
-          <div className={`${styles.responseBox}`}>
-            {/* Display the generated image */}
-            <div className={`${styles.imageBox}`}>
-              {generatedLink ? (
-                <img
-                  src={generatedLink}
-                  alt="Generated Image"
-                  className={`${styles.generatedImage}`}
-                />
-              ) : (
-                <p style={{ color: "gray", textAlign: "center", content: "center", marginTop: "20px" }}> Your image will display here</p>
-              )
-              }
-            </div>
-            <div className={`${styles.buton}`}>
-              <button className={`${styles.generateButtons}`}
-                onClick={handleCopyImage}
-              >
-                Copy Image
-              </button>
-              <button className={`${styles.generateButtons}`}
-                onClick={handleDownloadImage}
-              >
-                Download Image
-              </button>
-
-              <button className={`${styles.generateButtons}`}
-                onClick={handleCopylink}
-              >
-                Copy Image Link
-              </button>
-
-            </div>
-          </div>
-
 
         </div>
+      <  div className={`${styles.centerContent}`}>
+           
+        <button className={`${styles.button}`} >
+          Upload File
+        </button>
+</div>
+
+        <div className={`${styles.headings}`}>Response</div>
+
+        <div className={`${styles.boxWithContents}`}>
+          <div className={`${styles.centerContent}`}>
+            
+            <p className={`${styles.ptag}`}>
+              your text
+            </p>
+          </div>
+
+        </div>
+        <  div className={`${styles.centerContent}`}>
+           
+           <button className={`${styles.button}`} >
+             copy text
+           </button>
+   </div>
 
       </div>
       <div className={`${styles.reserve}`}>
